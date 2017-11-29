84ac81efa6870c276eaff445af004e5b8b040258
Bennett White <benwh1te@vt.edu>
2017-11-29 17:02:36 -0500

    fuck presentations

--- website.py
+++ website.py
@@ -1,11 +1,13 @@
 import flask
 from pymongo import MongoClient
 import os
+#import face_recognition
 
 client = MongoClient()
 db = client.hokieSports
 teams = db.teams
 users = db.users
+interactions = db.interactions
 
 app = flask.Flask(__name__)
 
@@ -38,28 +40,31 @@ app = flask.Flask(__name__)
 def homepage():
     return flask.render_template('main.html', test='test')
 
-@app.route('/checkin', methods=['GET'])
-def checkIn():
-    return flask.render_template('checkIn.html')
+# @app.route('/checkin', methods=['GET'])
+# def checkIn():
+#     return flask.render_template('checkIn.html')
 
 @app.route('/newMember')
 def newMember():
-    teamArr = []
-    for team in teams.find():
-        teamArr.append(str(team['teamName'] + ' - ' + team['sport']))
-    return flask.render_template('newMember.html', teams=teamArr)
+    return flask.render_template('newMember.html')
 
 @app.route('/newTeam')
 def newTeam():
     sports = ['Soccer', 'Football']
     return flask.render_template('newTeam.html', sports=sports)
+@app.route('/joinTeam')
+def join():
+    teamArr = []
+    for team in teams.find():
+        teamArr.append(str(team['teamName'] + ' - ' + team['sport']))
+    return flask.render_template('joinTeam.html', teams=teamArr)
 
-@app.route('/handleCheckInData', methods=['POST'])
-def handleCheckInData():
-    #return flask.render_template('checkIn.html')
-    print(flask.request.form['pidNumber'])
-    return flask.redirect('http://localhost:5000/checkin')
-    #check database
+# @app.route('/handleCheckInData', methods=['POST'])
+# def handleCheckInData():
+#     #return flask.render_template('checkIn.html')
+#     print(flask.request.form['pidNumber'])
+#     return flask.redirect('http://localhost:5000/checkin')
+#     #check database
 
 @app.route('/handleNewTeamData', methods=['POST'])
 def handleNewTeamData():
@@ -68,6 +73,8 @@ def handleNewTeamData():
     if teamName == '' or teamCaptain == '':
         return 'Error - fill in all data forms'
     sport = flask.request.form['sport']
+    if teams.find_one({'teamName': teamName, 'sport': sport}) != None:
+        return 'Error - Team Name already exists'
     data = dict()
     data['teamName'] = teamName
     data['teamCaptain'] = teamCaptain
@@ -79,17 +86,35 @@ def handleNewTeamData():
     #level = flask.request.form['level']
     #add to database
 
+#change to register
+#then set up new page to join a team
 @app.route('/handleNewMemberData', methods=['POST'])
 def handleNewMemberData():
     playerName = flask.request.form['playerName']
     pidNumber = flask.request.form['pidNumber']
     email = flask.request.form['email']
-    teamName = flask.request.form['teamName']
     f = flask.request.files['myPhoto']
-    filename = 'test.jpg'
-    f.save(os.path.join('./test.jpg'))
+
+    if playerName == '' or pidNumber == '' or email == '' or f.filename == '':
+        return "Error - fill in all data forms"
+
+    if '.jpg' not in f.filename or '.jpeg' not in f.filename:
+        return 'Error - must upload a file in .jpg or .jpeg format'
+
+
+    filename = 'face.jpg'
+    f.save(os.path.join('./face.jpg'))
+    os.remove('./face.jpg')
+    #load = face_recognition.load_image_file('face.jpg')
+    #encoding = face_recognition.face_encodings(load)[0]
+
+
     return flask.redirect('http://localhost:5000/')
     #add to database
+
+@app.route('/handleJoinTeam', methods=['POST'])
+def joinTeam():
+    return flask.redirect('http://localhost:5000/')
 
 @app.route('/getInfo', methods=['GET'])
 def getInfo():

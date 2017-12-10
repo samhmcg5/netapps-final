af5a4d3ad00689138a486b601967e454bff813b9
Samuel McGhee <samhmcg5@vt.edu>
2017-12-10 16:41:09 -0500

    69

--- sam_testing/pi1_v2.py
+++ sam_testing/pi1_v2.py
@@ -60,80 +60,14 @@ def stop_cam():
     video.end_capture()
     return "Stopping"
 
+@app.route('/shit')
+def break_it():
+    global video
+    video.exit = True
+    return send_file('yousuck.jpg')
 
-app.run(host='0.0.0.0', port=9999, debug=True)
+
+app.run(host='192.168.1.105', port=9999, debug=True)
 video.start()
 
 
-
-############################
-### DATA TRANSFER TO PI2 ###
-############################
-# class TransferThread(Thread):
-#     def __init__(self, ip, port_c, port_img):
-#         self.ip = ip
-#         self.port_c = port_c
-#         self.port_img = port_img
-#         self.ctl_sock = socket.socket()
-#         self.img_sock = socket.socket()
-#         self.exit = False
-#         Thread.__init__(self)
-#         self.video = DisplayThread()
-
-#     def connect_sockets(self):
-#         print("Connecting to server")
-#         self.ctl_sock.connect((self.ip, self.port_c))
-#         self.img_sock.connect((self.ip, self.port_img))
-
-#     def send_image(self):
-#         print("sending image")
-#         frame = self.video.get_frame()
-#         cv2.imwrite("image.jpg", frame)
-#         f = open("image.jpg",'rb')
-#         img = f.read(1024)
-#         while (img):
-#             self.img_sock.send(img)
-#             img = f.read(1024)
-#         f.close()
-
-#     def close_camera(self):
-#         print("Closing Camera")
-#         self.video.end_capture()
-
-#     def start_camera(self):
-#         print("Starting Camera")
-#         self.video.begin_capture()
-
-#     def recv_control(self):
-#         text = ''
-#         data = ''
-#         while data is not '!':
-#             data = self.ctl_sock.recv(1).decode()
-#             text += data
-#         return text[:-1]
-
-#     def run(self):
-#         self.video.start()
-#         print("Starting TransferThread")
-#         while not self.exit:
-#             text = self.recv_control()
-#             print(text)
-#             if text == 'image':
-#                 self.send_image()
-#             elif text == 'approved':
-#                 self.close_camera()
-#             elif text == 'denied':
-#                 self.close_camera()
-#             elif text == 'start':
-#                 self.start_camera()
-#             elif text == 'exit':
-#                 break
-
-
-#########################
-###        MAIN       ###
-#########################
-# if __name__ == '__main__':
-#     xfer = TransferThread('localhost', 9999, 9998)
-#     xfer.connect_sockets()
-#     xfer.start()

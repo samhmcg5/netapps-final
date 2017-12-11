1b213b5da098316ee28ae13e2dcfd52a29381dec
Eric Simpson <esimp12@vt.edu>
2017-12-11 11:47:58 -0500

    phone rest api getting db data

--- camera_rest.py
+++ camera_rest.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 from flask_bootstrap import Bootstrap 
-import face_recognition
+#import face_recognition
 import requests
 import time
 import os
@@ -15,14 +15,14 @@ def login():
 	if request.method == 'POST':
 		if request.form['email'] == 'esimp12@vt.edu' and request.form['password'] == 'admin':
 			session['logged_in'] = True
-			session['user'] = 'eric'
+			session['user'] = request.form['email']
 			return redirect(url_for('home'))		
 	return render_template('index.html')
 
 @app.route('/home', methods=['GET', 'POST'])
 def home():
-	filename = "/static/" + session.get('user') + ".jpg"
 	filename = "/static/camera_image.jpg"
+	filename = "/static/eric.jpg"
 	if not session.get('logged_in'):
 		return redirect(url_for('login'))
 	if request.method == 'POST':
@@ -32,7 +32,7 @@ def home():
 			return redirect(url_for('login'))
 		elif request.form['action'] == 'photo':
 			# Send initialize request
-			r = requests.get("http://192.168.1.105:9999/")
+			"""r = requests.get("http://192.168.1.105:9999/")
 			time.sleep(1)
 			# Start camera request
 			r = requests.get("http://192.168.1.105:9999/start")
@@ -48,10 +48,50 @@ def home():
 			time.sleep(1)
 
 			# Stop the camera request
-			r = requests.get("http://192.168.1.105:9999/stop")
+			r = requests.get("http://192.168.1.105:9999/stop")"""
 
-			return render_template('home.html', filename=filename)
+			pid = session.get('user')
+			pid = pid.split("@")[0]
+			pid = "benwh1te"
+			str1 = "http://192.168.1.130:5000/getInfo?pid=%s" % (pid)
+			r = requests.get(str1)
+			json_data = r.json()
+
+			print(json_data)
+
+			status = json_data['regStatus']
+			statusName = None
+			player = None
+			sport = None
+			team = None
+			location = None 
+			opponent = None
+			timeStr = None
+			if status != 0:
+				player = json_data['playerName']
+				sport = json_data['sport']
+				team = json_data['teamName']
+				if status == 1:
+					statusName = "Registered and Not Paid"
+				elif status == 2:
+					statusName = "Registered and Paid"
+				nextGame = json_data['nextGame']
+				timeStr = time.asctime(time.struct_time(nextGame['time']))
+				location = nextGame['location']
+				opponent = nextGame['opponent']
+
+				# encoding stuff here
+
+			else:
+				statusName = "Not Registered"
+
+			if player is not None and sport is not None and team is not None and location is not None and opponent is not None and timeStr is not None:
+				return render_template('home.html', filename=filename, player=player, sport=sport, team=team,
+				status=statusName, time=timeStr, location=location, opponent=opponent)
+			else: 
+				return render_template('home.html', status=statusName)
+
 	return render_template('home.html')
 
 if __name__ == '__main__':
-	app.run(host="192.168.1.125")
+	app.run()
